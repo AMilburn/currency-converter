b230ff65c852e84369450e16a67a296d892ed408
Amanda Milburn <amanda.milburn@nike.com>
2021-06-24 16:36:34 +0200

    fix(api/conversion): upgrade to https

--- src/api/conversion.ts
+++ src/api/conversion.ts
@@ -1,7 +1,7 @@
 export const getConversionRates = async (currToConvert: string, finalCurr: string) => {
   const apiKey = process.env.REACT_APP_API_KEY;
   try {
-    const conversionApiUrl = `http://api.exchangeratesapi.io/v1/latest?access_key=${apiKey}`
+    const conversionApiUrl = `https://api.exchangeratesapi.io/v1/latest?access_key=${apiKey}`
     const res = await fetch(conversionApiUrl);
     const resObj = await res.json();
     return [
